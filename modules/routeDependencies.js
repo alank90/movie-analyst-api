c58440d602bfc0b7c1988d3ecff86eafce7eff97
akillian@outlook.com <akillian@outlook.com>
2017-10-29 18:22:35 -0400

    commit

--- modules/routeDependencies.js
+++
@@ -1,9 +0,0 @@
-var dependencies =  '// Dependencies Declarations';
-dependencies += "const bodyParser = require('body-parser'')";
-
-dependencies += "// body-parser allows express to go into request body and extract";
-dependencies += "// the json data and exposes it thru req.body";
-dependencies += "router.use(bodyParser.json());";
-dependencies += "router.use(bodyParser.urlencoded({ extended: false }));";
-
-module.exports = dependencies;
