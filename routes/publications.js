5b9eca878905e1ee66e9f82bea9eb99a94c49555
Alan K <akillian@outlook.com>
2017-10-30 13:57:32 -0400

    commit

--- routes/publications.js
+++ routes/publications.js
@@ -1,6 +1,13 @@
 // Dependencies Declarations
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+
+// body-parser allows express to go into request body and extract 
+// the json data and exposes it thru req.body
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
 
 // Implement the publications API endpoint
 router.get('/', function (req, res) {
@@ -14,5 +21,30 @@ router.get('/', function (req, res) {
          res.json(publications);      
        });
  });
+
+ /*
+ * Put to Update publications.
+ */
+router.put('/updatepublication/:id', function(req, res) {
+    const db = req.db;
+
+    const publicationCollection = db.get("publications");
+    const publicationToUpdate = req.params.id; // Assign collection document id from url :id value
+    const publicationName = req.body.name;
+    const publicationAvatar = req.body.avatar;
+    
+    //  Update the Publisher mongo document from put parameters
+    publicationCollection.update({'_id': publicationToUpdate},
+        {
+            $set: {
+              name: publicationName,
+              avatar: publicationAvatar
+            }
+        },
+        function(err)  {
+          res.send((err === null) ? {msg: ''} : {msg: err});
+        }); // publication .update
+
+}); // End router.put
 
  module.exports = router;
