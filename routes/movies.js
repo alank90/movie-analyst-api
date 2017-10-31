c5f34511d5a66fa1ba80267a07eeba0f7d2420a9
Alan K <akillian@outlook.com>
2017-10-31 13:20:54 -0400

    Commit API working fully

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,20 @@
 // Dependencies Declarations
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const bodyParser = require('body-parser');
+const morgan = require('morgan'); // enhance logger
 
 // body-parser allows express to go into request body and extract 
 // the json data and exposes it thru req.body
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
+
+// You can set morgan to log differently depending on your environment
+if (router.get('env') == 'production') {
+  router.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../morgan.log' }));
+} else {
+  router.use(morgan('dev'));
+}
 
 // Implement the /movies API endpoint
 router.get('/', function(req, res) {
--- routes/pending.js
+++ routes/pending.js
@@ -1,6 +1,21 @@
 // Dependencies Declarations
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// body-parser allows express to go into request body and extract 
+// the json data and exposes it thru req.body
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+
+// You can set morgan to log differently depending on your environment
+if (router.get('env') == 'production') {
+  router.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../morgan.log' }));
+} else {
+  router.use(morgan('dev'));
+}
 
 // Implement the pending reviews API endpoint
 router.get('/', function (req, res) {
@@ -14,5 +29,36 @@ router.get('/', function (req, res) {
          res.json(pending);      
        });
 });
+
+/*
+ * Put to Update pending movies.
+ */
+router.put('/updatepending/:id',function(req, res){
+    const db = req.db;
+   
+    const pendingCollection = db.get("pending");
+    const pendingMovieToUpdate = req.params.id;
+    const pendingTitle = req.body.title;
+    const pendingRelease = req.body.release;
+    const pendingScore = req.body.score;
+    const pendingReviewer = req.body.reviewer;
+    const pendingPublication = req.body.publication;
+
+     // Update Pending document from put info
+    pendingCollection.update({'_id': pendingMovieToUpdate},
+        {
+            $set: {
+              title: pendingTitle,
+              release: pendingRelease,
+              score: pendingScore,
+              reviewer: pendingReviewer,
+              publication: pendingPublication
+            }
+        },
+        function(err)  {
+        res.send((err === null) ? {msg: ''} : {msg: err});
+    }); // pendingCollection .update
+
+});       
 
 module.exports = router;
